f4b40d9d00818040e570efa35470d50860f26550
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2022-06-13 22:33:30 +0700

    chore: update nginx

--- perf_test.py
+++ perf_test.py
@@ -1,3 +1,4 @@
+import random
 from locust import HttpUser, between, task
 
 NPM = "1906350616"
@@ -6,5 +7,10 @@ class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
     
     @task
-    def index(self):
+    def get_mahasiswa_no_cache(self):
         self.client.get(f"/read/{NPM}")
+
+    @task
+    def get_mahasiswa_with_cache(self):
+        trx_id = random.randint(1, 100)
+        self.client.get(f"/read/{NPM}/99")
